a6494f029225d235d78175718ef5c3a0df6065e6
suemor <3100825062@qq.com>
2022-04-05 16:28:33 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  base: './',
   resolve: {
     alias: {
       "@": resolve(__dirname, "/src"),
